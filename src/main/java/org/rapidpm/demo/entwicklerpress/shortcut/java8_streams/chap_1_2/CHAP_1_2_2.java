42c386b3df43dd2b17e0936ba9a8c5afbc0b28df
Sven Ruppert <sven.ruppert@rapidpm.org>
2013-11-22 10:34:33 +0100

    fixed double usage of Stream

--- src/main/java/org/rapidpm/demo/entwicklerpress/shortcut/java8_streams/chap_1_2/CHAP_1_2_2.java
+++ src/main/java/org/rapidpm/demo/entwicklerpress/shortcut/java8_streams/chap_1_2/CHAP_1_2_2.java
@@ -16,18 +16,11 @@ import org.rapidpm.demo.entwicklerpress.shortcut.java8_streams.PairListGenerator
 public class CHAP_1_2_2 {
     public static void main(String[] args) {
         final List<Pair> generateDemoValues = new PairListGenerator(){}.generateDemoValues();
+        final Stream<DemoElement> demoElementStream = getDemoElementStream(generateDemoValues);
 
-        //map from Point to DemoElements
-        final Stream<DemoElement> demoElementStream = generateDemoValues.stream().map(v -> {
-            final String value = v.getValue();
-            final DemoElement d = new DemoElement();
-            d.setDatum(new Date());
-            d.setValue(Base64.getEncoder().encodeToString(value.getBytes()));
-            return d;
-        });
 
-        final Stream<String> stringStream = demoElementStream.map(v -> v.getValue());
-        final Stream<String> stringStreamShort = demoElementStream.map(DemoElement::getValue);
+        final Stream<String> stringStream = getDemoElementStream(generateDemoValues).map(v -> v.getValue());
+        final Stream<String> stringStreamShort = getDemoElementStream(generateDemoValues).map(DemoElement::getValue);
 
         //map from Point to DemoElements to Strings
         final List<String> stringList = generateDemoValues.stream().map(v -> {
@@ -40,10 +33,16 @@ public class CHAP_1_2_2 {
 
         final Stream<Pair> filteredPairStream = generateDemoValues.stream().filter(v -> v.getId() % 2 == 0);
 
+    }
 
-
-
-
-
+    private static Stream<DemoElement> getDemoElementStream(List<Pair> generateDemoValues) {
+        //map from Point to DemoElements
+        return generateDemoValues.stream().map(v -> {
+            final String value = v.getValue();
+            final DemoElement d = new DemoElement();
+            d.setDatum(new Date());
+            d.setValue(Base64.getEncoder().encodeToString(value.getBytes()));
+            return d;
+        });
     }
 }
